d3d279c8e4b9b1f68f5fbe5353aa891028812545
Mohammed Amaan Mahmood Khan <150614816+curious-ferret@users.noreply.github.com>
2024-08-12 23:28:02 +0530

    Update SET_1A.c

--- SET_1A.c
+++ SET_1A.c
@@ -12,7 +12,7 @@ int main(int argc, char * argv[])
 
     if(argc != 3)
     {
-        printf("Usage: ./a.exe sourcefile destinationfile.\n");
+        printf("Usage: ./a.out sourcefile destinationfile.\n");
         exit(1);
     }
 
