224891c4d7be4d8899540154f84976da0112acc6
Mohammed Amaan Mahmood Khan <150614816+curious-ferret@users.noreply.github.com>
2024-08-17 12:30:22 +0000

    Changes to be committed: 	modified:   SET_1B.c

--- SET_1B.c
+++ SET_1B.c
@@ -1,25 +1,34 @@
 //Un-named pipes
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 int main(void)
 {
-    int pid, a[2];
-    char str[30], buff[30];
+    int pid, a[2], b[2];
+    char str[30], buff1[30], buff2[30];
     system("clear");
 
     pipe(a);
+    pipe(b);
     pid = fork();
     if(pid == 0)
     {
-        strcpy(str, "Demo Output");
+        //to specify the size in read and write function, you can use either
+        //strlen(x) + 1  or  sizeof(x)
+        //where x is a char array
+        strcpy(str, "child says hi");
         write(a[1], str, strlen(str) + 1);
-        printf("Child process has written on pipe a.\n");
+        sleep(2);
+        read(b[0], buff1, sizeof(buff1));
+        printf("(through pipe b)\nResponse from parent is '%s'\n", buff1);
     }
     else
     {
-        read(a[0], buff, sizeof(buff));
-        printf("Parent process has recieved %s through the read pipe.\n", buff);
+        read(a[0], buff2, sizeof(buff2));
+        printf("(through pipe a)\nRequest from child process is '%s'.\n", buff2);
+        strcpy(buff2, "parent says hello");
+        write(b[1], buff2, strlen(buff2) + 1);
     }
 }
