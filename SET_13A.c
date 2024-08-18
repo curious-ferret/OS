2b30fdda0786c12cd226edc6de15e5e98c551ea5
Mohammed Amaan Mahmood Khan <150614816+curious-ferret@users.noreply.github.com>
2024-08-18 07:39:51 +0000

    Changes to be committed: 	new file:   SET_13A.c

---
+++ SET_13A.c
@@ -0,0 +1,53 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<limits.h> 
+int main(void)
+{
+    int req[20], n, hdpos, tseek = 0, i, temp[20], min, dist, index, cpos, cnt = 0; 
+    float avgmv;
+    system("clear");
+
+    printf("Enter the number of requests: "); 
+    scanf("%d", &n);
+
+    printf("Enter the disk read requests\n"); 
+    for (i = 0; i < n; i++)
+    {
+        printf("Request %d: ", i + 1); 
+        scanf("%d", &req[i]);
+        temp[i] = req[i];
+    }
+    printf("Enter head position: "); 
+    scanf("%d", &hdpos);
+
+    printf("SSTF sequence is:\n"); 
+    //cpos: current position
+    cpos = hdpos;
+
+    while (cnt < n)
+    {
+        min = INT_MAX; 
+        for (i = 0; i < n; i++)
+        {
+            if (temp[i] != -1)
+            {
+                dist = abs(req[i] - cpos); 
+                if (min > dist)
+                {
+                    min = dist; 
+                    index = i; 
+                }
+            }
+        }
+        tseek += min;
+        temp[index] = -1; 
+        cnt++; 
+        cpos = req[index];
+        printf("%d -> ", req[index]); 
+        if (cnt == n)
+            break;
+    }
+    avgmv = (float) tseek / n; 
+    printf("\nTotal Seek Time = %d\n", tseek); 
+    printf("Average Movement = %.2f\n", avgmv);
+}
